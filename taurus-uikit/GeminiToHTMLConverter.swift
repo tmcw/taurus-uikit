3b8762a4dada6fef7525737cc92fa21e0b130e7e
Tom MacWright <tom@macwright.org>
2021-02-13 16:29:58 -0800

    Basic URL loading

--- taurus-uikit/GeminiToHTMLConverter.swift
+++ taurus-uikit/GeminiToHTMLConverter.swift
@@ -9,29 +9,83 @@ import Foundation
 import Html
 
 func geminiToHTML(page: Page) -> String {
-    var html = ""
-
     let nodes: [Html.Node] = page.document!.tree.children.map { (node) -> Html.Node in
         switch node.data {
         case Data.root:
-            return Html.Node.br;
+            return Html.Node.br
         case Data.brk:
-            return Html.Node.br;
+            return Html.Node.br
         case let .listItem(value):
             return Html.Node.ul(.li(.text(value)))
         case let .text(value):
             return Html.Node.p(.text(value))
         case let .heading(value, rank):
-            return Html.Node.h1(.text(value))
+            switch rank {
+            case 1:
+                return Html.Node.h1(.text(value))
+            case 2:
+                return Html.Node.h2(.text(value))
+            default:
+                return Html.Node.h3(.text(value))
+            }
+
         case let .quote(value):
             return Html.Node.blockquote(.text(value))
         case let .pre(value, _):
             return Html.Node.pre(.text(value))
         case let .webLink(value, url):
-            return Html.Node.a(.text(value))
+            return Html.Node.div(Html.Node.a(
+                attributes: [.href(url.absoluteString)],
+                .text(value)
+            ))
         case let .link(value, url):
-            return Html.Node.a(.text(value))
+            return Html.Node.div(Html.Node.a(
+                attributes: [.href(url.absoluteString)],
+                .text(value)
+            ))
         }
     }
-    return Html.render(Html.Node.document(Html.Node.html(.body(.fragment(nodes)))));
+    return Html.render(
+        Html.Node.document(
+            Html.Node.html(
+                .head(
+                    .meta(viewport: .width(.deviceWidth), .initialScale(1)),
+                    .style(safe: """
+body {
+  font-family: -apple-system-ui-serif, ui-serif;
+  background: #111;
+  color: #999;
+  padding: 10px;
+}
+
+div {
+  padding: 5px 0;
+}
+
+p {
+  line-height: 1.625;
+}
+
+blockquote {
+  padding: 5px;
+  font-style: italic;
+}
+
+pre {
+  overflow-x: auto;
+  font-family: -apple-system-mono;
+}
+
+h1, h2, h3 {
+  color: #fff;
+}
+
+a {
+  color: #0074bf;
+}
+"""
+                    )
+                ),
+                .body(.fragment(nodes))
+            )))
 }
--- taurus-uikit/ViewController.swift
+++ taurus-uikit/ViewController.swift
@@ -5,38 +5,53 @@
 //  Created by Tom MacWright on 2/5/21.
 //
 
-import UIKit
 import Network
+import UIKit
 import WebKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, WKNavigationDelegate {
     private var tcpStreamAlive: Bool = false
     private var online: Bool = false
-    private var page: Page = Page(url: nil, status: PageStatus.none, document: nil, source: "")
+    private var page = Page(url: nil, status: PageStatus.none, document: nil, source: "")
     private var content: String = ""
 
     @IBOutlet var addressBar: UITextField!
     @IBOutlet var webView: WKWebView!
-    
+
     @IBAction func enteredAddress(_ sender: UITextField) {
         loadUrl(inputUrl: sender.text!)
     }
-    
+
     private func drawPage() {
-        print(geminiToHTML(page: self.page))
-        webView.loadHTMLString(geminiToHTML(page: self.page), baseURL: URL(string: "http://localhost"))
+        webView.navigationDelegate = self
+        webView.loadHTMLString(geminiToHTML(page: page), baseURL: URL(string: "http://localhost"))
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print(navigationAction.request.url)
+        if navigationAction.request.url?.absoluteString == "http://localhost/" {
+            decisionHandler(.allow)
+            return
+        }
+        if let scheme = navigationAction.request.url?.scheme {
+            if scheme == "gemini" {
+                loadUrl(inputUrl: navigationAction.request.url!.absoluteString)
+                decisionHandler(.cancel)
+                return
+            }
+        }
+        decisionHandler(.cancel)
+    }
+
     // https://stackoverflow.com/questions/54452129/how-to-create-ios-nwconnection-for-tls-with-self-signed-cert
-    func createTLSParameters(allowInsecure: Bool, queue: DispatchQueue) -> NWParameters {
+    func createTLSParameters(allowInsecure _: Bool, queue: DispatchQueue) -> NWParameters {
         let options = NWProtocolTLS.Options()
-        sec_protocol_options_set_verify_block(options.securityProtocolOptions, { (sec_protocol_metadata, sec_trust, sec_protocol_verify_complete) in
+        sec_protocol_options_set_verify_block(options.securityProtocolOptions, { _, sec_trust, sec_protocol_verify_complete in
             let trust = sec_trust_copy_ref(sec_trust).takeRetainedValue()
             var error: CFError?
             sec_protocol_verify_complete(true)
@@ -54,54 +69,54 @@ class ViewController: UIViewController {
         }, queue)
         return NWParameters(tls: options)
     }
-    
+
     private func loadUrl(inputUrl: String) {
-        self.content = ""
+        content = ""
         print("turning \(inputUrl) into a url…")
-        // todo: safe unwrap
-        let u = URL(string: inputUrl)!;
-        
-        if (u.scheme != "gemini") {
+        // TODO: safe unwrap
+        let u = URL(string: inputUrl)!
+
+        if u.scheme != "gemini" {
             // Exit
-            return;
+            return
         }
-        page.url = URL(string: inputUrl);
-        let host = u.host;
-        
+        page.url = URL(string: inputUrl)
+        let host = u.host
+
         let queue = DispatchQueue(label: "taurus")
-        let hostEndpoint = NWEndpoint.Host.init(host!)
+        let hostEndpoint = NWEndpoint.Host(host!)
         let nwConnection = NWConnection(
             host: hostEndpoint,
             port: 1965,
             using: createTLSParameters(allowInsecure: true, queue: queue)
         )
-        nwConnection.stateUpdateHandler = self.stateDidChange(to:)
-        self.setupReceive(on: nwConnection)
-        
+        nwConnection.stateUpdateHandler = stateDidChange(to:)
+        setupReceive(on: nwConnection)
+
         nwConnection.start(queue: queue)
         print("Sending \(inputUrl) to host")
-        nwConnection.send(content: "\(inputUrl)\r\n".data(using: .utf8)!, completion: .contentProcessed( { error in
+        nwConnection.send(content: "\(inputUrl)\r\n".data(using: .utf8)!, completion: .contentProcessed { error in
             print("data sent")
             if let error = error {
                 print("got error")
                 // self.connectionDidFail(error: error)
                 return
             }
-        }))
+        })
     }
-    
+
     private func setupReceive(on connection: NWConnection) {
-        connection.receive(minimumIncompleteLength: 1, maximumLength: 65536) { (data, contentContext, isComplete, error) in
+        connection.receive(minimumIncompleteLength: 1, maximumLength: 65536) { data, _, isComplete, error in
             // Read data off the stream
             if let data = data, !data.isEmpty {
                 self.content += String(decoding: data, as: UTF8.self)
             }
-            
+
             if isComplete {
                 connection.cancel()
                 self.tcpStreamAlive = false
-                self.page.source = self.content;
-                self.page.document = parseResponse(content: self.content);
+                self.page.source = self.content
+                self.page.document = parseResponse(content: self.content)
             } else if let error = error {
                 print("setupReceive: error \(error.localizedDescription)")
             } else {
@@ -109,42 +124,38 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+
     private func stateDidChange(to state: NWConnection.State) {
         switch state {
         case .setup:
-            self.notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "setup")
-            self.tcpStreamAlive = true
-            break
+            notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "setup")
+            tcpStreamAlive = true
         case .waiting:
-            self.notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "waiting")
-            self.tcpStreamAlive = true
-            break
+            notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "waiting")
+            tcpStreamAlive = true
         case .ready:
-            self.notifyDelegateOnChange(newStatusFlag: true, connectivityStatus: "ready")
-            self.tcpStreamAlive = true
-            break
-        case .failed(let error):
+            notifyDelegateOnChange(newStatusFlag: true, connectivityStatus: "ready")
+            tcpStreamAlive = true
+        case let .failed(error):
             let errorMessage = "Error: \(error.localizedDescription)"
-            self.notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: errorMessage)
-            self.tcpStreamAlive = false
+            notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: errorMessage)
+            tcpStreamAlive = false
         case .cancelled:
-            self.notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "cancelled")
-            self.tcpStreamAlive = false
+            notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "cancelled")
+            tcpStreamAlive = false
             drawPage()
-            //self.setupNWConnection()
-            break
+        // self.setupNWConnection()
         case .preparing:
-            self.notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "preparing")
-            self.tcpStreamAlive = true
+            notifyDelegateOnChange(newStatusFlag: false, connectivityStatus: "preparing")
+            tcpStreamAlive = true
         }
     }
-    
+
     private func notifyDelegateOnChange(newStatusFlag: Bool, connectivityStatus: String) {
-        if newStatusFlag != self.online {
+        if newStatusFlag != online {
             print("newStatusFlag: \(newStatusFlag) - connectivityStatus: // \(connectivityStatus)")
             // self.networkStatusDelegate?.networkStatusChanged(online: newStatusFlag, // connectivityStatus: connectivityStatus)
-            self.online = newStatusFlag
+            online = newStatusFlag
         } else {
             print("connectivityStatus: \(connectivityStatus)")
         }
